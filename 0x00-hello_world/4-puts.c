2e499115a40e1d7b10ed75b6dc9a809c32abd9c6
Znaidi-Achraf <znaidiachraf96@gmail.com>
2023-06-15 11:19:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: A C program that prints with the puts function
+ *    * A C program that prints with the puts function
  *     *
  *      * Return: Always 0 (Success)
- *       */
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle\"");
 			return (0);
 }
 
