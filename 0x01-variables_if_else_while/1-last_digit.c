da9797ed935771a0497cd8e296e1de34c683e10c
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-17 02:06:25 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,35 +2,31 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
 /**
- * main - start portal
+ * main - Entry point
  *
- * Return: Success 0
- */
-/* betty style doc for function main - goes there */
+ * Description: print the value of n status:
+ *              greater than, is zero and is not less than 6.
+ *
+ * Return: Always O (Success)
+*/
+
 int main(void)
 {
-	int n;
-	int last_digit;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	last_digit = n % 10;
 
-	if (last_digit > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	}
-	else if (last_digit == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				n, last_digit);
-	}
+	/*my code*/
+	digit = n % 10; /*gets last digit*/
+
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+
 	return (0);
 }
