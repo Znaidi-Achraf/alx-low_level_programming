8efb367b45493d5bd4e304d2eed10960103e44be
Znaidi-Achraf <znaidiachraf96@gmail.com>
2023-06-16 18:42:07 -0700

    all files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+int		main(void)
+{
+	write(1, "hhhhhfghijklmnopqrstuvwxyz\n", 27);
+	return (0);
+}
