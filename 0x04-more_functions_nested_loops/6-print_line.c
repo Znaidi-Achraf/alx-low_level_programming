29dba9f868018ba5fa1bc965e59f7d589a8d9702
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-23 00:34:55 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ *
+ * Description: Prints lines
+ *@n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
