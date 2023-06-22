b300d39fe57d3e1349c3ab73be2ed19762382754
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-23 00:23:25 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
