a0477ff32eb8edd551899c3c0353d48464dc5641
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-22 02:36:29 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,3 +4,20 @@
  * main - prints the sum of all multiples² of 3 or 5 up to 102
  * return: success
  */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
