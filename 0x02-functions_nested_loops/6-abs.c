a7260b3fe856042e9804dd91d3e738a9c63f0e2d
Znaidi-Achraf <znaidiachraf96@gmail.com>
2023-06-21 17:39:20 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,2 +1,21 @@
+#include "main.h"
 
+/**
+ * _abs -return absolute value
+ *
+ * @i: int checked
+ *
+ * Return: the absolute value
+ */
 
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-1 * i);
+	}
+}
