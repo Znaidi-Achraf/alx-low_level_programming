48b536c5e39e5db11f6c10ce12e1fde7c1732680
Znaidi-Achraf <znaidiachraf96@gmail.com>
2023-06-21 19:31:02 -0700

    file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- *
- * Description: prints the first 50 Fibonacci numbers
- *              starting with 1 and 2 followed by a new line
+ * main - Entry point of the program
  *
  * Return: Always 0 (success)
  */
