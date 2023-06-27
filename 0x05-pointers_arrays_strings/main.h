fac9a4f9297e94618b83b75d3c91e03ae63d2545
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-27 14:07:27 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-#ifdef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -15,33 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
