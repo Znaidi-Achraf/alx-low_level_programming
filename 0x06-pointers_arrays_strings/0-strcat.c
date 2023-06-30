39910c732bc262c538f0f733204fbe32a5c5284c
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-06-30 21:12:58 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,3 +8,22 @@
  *
  * Return: pointer to resulting string @dest
  */
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
+}
