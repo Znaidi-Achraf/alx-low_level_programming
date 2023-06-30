694304880dfe0bbb670518bd628dbd400327c07b
Znaidi-Achraf <znaidiachraf96@gmail.com>
2023-06-30 13:11:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,10 @@
+#include "main.h"
 
+/**
+ * _strcat - function that concatrnates two string.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+ */
