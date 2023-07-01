a39f1d84c64542f96afee2eebe3b91bdc507ffbf
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-07-01 01:51:56 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,9 +2,24 @@
 
 /**
  * reverse_array -reverses the content of an array of integers.
- * @a: an array of intgers
+ * @a: an array of integers
  * @n: the number of elements to swap
  *
  * Return: nothing
  */
+void reverse_array(int *a, int n)
+{
+	int swap, begin, end;
+
+	begin = 0;
+	end = n - 1;
+	while (begin < end)
+	{
+		swap = *(a + begin);
+		*(a + begin) = *(a + end);
+		*(a + end) = swap;
+		begin++;
+		end--;
+	}
+}
 
