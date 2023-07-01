9d61c12ddf50cc3467584146896276f9680578eb
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-07-01 01:43:46 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
- * _strncat - a function that concatenates two string.
+ * _strncat - a function that concatenates two strings.
  *
- * @dest: pointer to destination input
+ * @dest: pointer to the destination input
  * @src: pointer to source input
  * @n: most number of bytes from @src
  *
  * Return: @dest
  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
+}
