5ce0a84477fc08d5eb0f697f4b8820473fffa6f2
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-07-01 02:07:06 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,3 +5,31 @@
  * @n: number tested
  * Return: always 0
  * /
+void print_number(int n)
+{
+	unsigned int val, div, c;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		val = n * -1;
+	}
+	else
+	{
+		val = n;
+	}
+
+	div = val;
+	c = 1;
+
+	while (div > 9)
+	{
+		div /= 10;
+		c *= 10;
+	}
+
+	for (; c >= 1; c /= 10)
+	{
+		_putchar(((val / c) % 10) + 48);
+	}
+}
