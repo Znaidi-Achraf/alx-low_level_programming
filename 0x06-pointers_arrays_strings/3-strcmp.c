d0f8ec037985d7aea9194aaa294f3c4384039c73
Znaidi-Achraf <132356500+Znaidi-Achraf@users.noreply.github.com>
2023-07-01 01:49:32 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,3 +6,19 @@
  * @s2: string 2
  * Return: 1 if true, 0 if false
  */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+	return (*s1 - *s2);
+	}
+}
